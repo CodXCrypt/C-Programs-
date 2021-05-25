90c5cd8b5321987cdc02e51c6d72fb6c0ea80049
Komal Gupta <komalgupta991000@gmail.com>
2021-05-25 12:59:34 +0530

    Create Ascii_value.cpp

---
+++ Ascii_value.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+	char c;
+	printf("Enter a character");
+	scanf("%c",&c);
+	//%displays the integer value of character
+	//%c displays the actual character
+	printf("ASCII value of %c=%d",c,c);
+	
+}
